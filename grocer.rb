4f3f5c3ba8b1d30ec6122b02fa8b82ae2b161a6c
Max Powell <max_f_powell@hotmail.co.uk>
2019-02-03 14:45:37 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -16,7 +16,6 @@ def consolidate_cart(cart)
 end
 
 def apply_coupons(cart, coupons)
-  binding.pry
   coupons.each do |coupon|
     cart["#{coupon[:item]} W/COUPON"] = {
       price: coupon[:cost],
