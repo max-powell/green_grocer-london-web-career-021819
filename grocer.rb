c4ac64bc6c7a355c2ff1f0dc24e2ed69ee2c5cd6
Max Powell <max_f_powell@hotmail.co.uk>
2019-02-03 14:55:54 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -19,6 +19,8 @@ def apply_coupons(cart, coupons)
   coupons.each do |coupon|
     if cart.keys.include?("#{coupon[:item]} W/COUPON")
       #old coupon
+      cart["#{coupon[:item]} W/COUPON"][:count] += 1
+      cart[coupon[:item]][:count] -= coupon[:num]
       binding.pry
     else
       #new coupon
