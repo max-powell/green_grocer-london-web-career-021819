0726901e965f42637ac05826460e7b6f6649affc
Max Powell <max_f_powell@hotmail.co.uk>
2019-02-03 15:48:17 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -17,19 +17,17 @@ end
 
 def apply_coupons(cart, coupons)
   coupons.each do |coupon|
-    if true
-      if cart.keys.include?("#{coupon[:item]} W/COUPON")
-        cart["#{coupon[:item]} W/COUPON"][:count] += 1
-        cart[coupon[:item]][:count] -= coupon[:num]
-      else
-        if cart.keys.include?(coupon[:item])
-          cart["#{coupon[:item]} W/COUPON"] = {
-            price: coupon[:cost],
-            clearance: cart[coupon[:item]][:clearance],
-            count: 1
-          }
-        cart[coupon[:item]][:count] -= coupon[:num]
-        end
+    if cart.keys.include?("#{coupon[:item]} W/COUPON")
+      cart["#{coupon[:item]} W/COUPON"][:count] += 1
+      cart[coupon[:item]][:count] -= coupon[:num]
+    else
+      if cart.keys.include?(coupon[:item])
+        cart["#{coupon[:item]} W/COUPON"] = {
+          price: coupon[:cost],
+          clearance: cart[coupon[:item]][:clearance],
+          count: 1
+        }
+      cart[coupon[:item]][:count] -= coupon[:num]
       end
     end
   end
